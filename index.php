0517cfaaa8598acc5167d0d5c7d93f6a6fbbdcdf
Servekt <113783392+Servekt@users.noreply.github.com>
2022-11-16 22:16:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<htlm>
+<head>
+        <title>D4G</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/cssSite5.css">
+    </head>     
+    <body> 
+<div class="box"><center>
+<form action="mailto:equipe404_D4G?subject=Demande de devis" name="envoi" method=POST enctype="text/plain">
+<table border="0">
+<b>Nom : </b><input name="nom"size=50 maxlength=50 class="texte">
+
+<b>Prenom : </b><input name="PRENOM"size=50 maxlength=50 class="texte"><br>
+<input type="submit" value="Envoyer" class="bouton">
+<input type="reset" name="Submit" value="Effacer" class="bouton">
+</form>
+</div>
+</body>
+<hr>
+</center>
+</htlm>
